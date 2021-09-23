580c924e890e2866da34ad3e21b439a329a286ed
aero127 <arnoldvdijk@hotmail.com>
2021-09-23 13:58:07 +0200

    update

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import { DateRangePicker } from 'react-date-range';
 import { useRouter } from "next/dist/client/router";
-import { roundToNearestMinutes } from "date-fns";
+
 
 
 
