0e7a40eefee43fd9a94bc72bfe2229c7754e3876
aero127 <arnoldvdijk@hotmail.com>
2021-09-24 09:59:28 +0200

    another update

--- next.config.js
+++ next.config.js
@@ -6,5 +6,9 @@ module.exports = {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
         ignoreDuringBuilds: true,
-      },
+    },
+    env: {
+        mapbox_key: "pk.eyJ1Ijoic3RydXVuZXIiLCJhIjoiY2t0d3p6dWUxMThiaTJxcW43d3Jud21sdSJ9.l8NnZiZbyOacBLMpQUj0Rg",
+    },
+
 };
